2d3d0e94b0ff5703702e1c0bc1b861dfe0061fe9
Xiumo Zhan <xiumozhan@gmail.com>
2017-02-18 00:17:23 -0800

    Cannot use require now, wrap these 2 modules in angular modules instead

--- js/aiController.js
+++ js/aiController.js
@@ -1,108 +1,106 @@
 'use strict';
 
-const gameState = require('gameState');
-
-//when the computer is 'x', decide what best possible result it can get in the current situation
-var maximizeScore = function(chessBoard, depth, alpha, beta) {
-    var gameResult = gameState.getState(chessBoard);
-    var isGameOver = (gameResult === gameState.resultMap.win) || (gameResult === gameState.resultMap.lose) || (gameResult === gameState.resultMap.draw);
-    //beta cut off
-    if(beta <= alpha) {
-        return gameResult;
-    }
-    if(depth === 0 || isGameOver) {
-        return gameResult;
-    }
-    var bestPossibleScore = -Infinity;
-    for(var i = 0; i < chessBoard.length; i++) {
-        if(chessBoard[i] === 'empty') {
-            chessBoard[i] = 'x';
-            //the more 'o' can get, the happier 'x' becomes
-            bestPossibleScore = Math.max(bestPossibleScore, minimizeScore(chessBoard, depth - 1, Math.max(bestPossibleScore, alpha), beta));
-            chessBoard[i] = 'empty';
+gameApp.service('aiController', ['gameState', function(gameState) {
+    //when the computer is 'x', decide what best possible result it can get in the current situation
+    var maximizeScore = function(chessBoard, depth, alpha, beta) {
+        var gameResult = gameState.getState(chessBoard);
+        var isGameOver = (gameResult === gameState.resultMap.win) || (gameResult === gameState.resultMap.lose) || (gameResult === gameState.resultMap.draw);
+        //beta cut off
+        if(beta <= alpha) {
+            return gameResult;
         }
-    }
-    return bestPossibleScore;
-};
-
-//when the computer is 'o', decide what best possible result it can get in the current situation
-var minimizeScore = function(chessBoard, depth, alpha, beta) {
-    var gameResult = gameState.getState(chessBoard);
-    var isGameOver = (gameResult === gameState.resultMap.win) || (gameResult === gameState.resultMap.lose) || (gameResult === gameState.resultMap.draw);
-    //alpha cut off
-    if(beta <= alpha) {
-        return gameResult;
-    }
-    if(depth === 0 || isGameOver) {
-        return gameResult;
-    }
-    var bestPossibleScore = Infinity;
-    for(var i = 0; i < chessBoard.length; i++) {
-        if(chessBoard[i] === 'empty') {
-            chessBoard[i] = 'o';
-            //the less 'x' can get, the happier 'o' becomes
-            bestPossibleScore = Math.min(bestPossibleScore, maximizeScore(chessBoard, depth - 1, alpha, Math.min(bestPossibleScore, beta)));
-            chessBoard[i] = 'empty';
+        if(depth === 0 || isGameOver) {
+            return gameResult;
         }
-    }
-    return bestPossibleScore;
-};
-
-// when computer is chosen to be 'x', how should it make its best next move
-var xMiniMax = function(chessBoard, depth) {
-    var bestMoves = new Array(chessBoard.length);
-    var index = 0;
-    var bestPossibleScore = -Infinity;
-    for(var i = 0; i < chessBoard.length; i++) {
-        if(chessBoard[i] === 'empty') {
-            chessBoard[i] = 'x';
-            var possibleScore = minimizeScore(chessBoard, depth, -Infinity, Infinity);
-            if(possibleScore > bestPossibleScore) {
-                bestPossibleScore = possibleScore;
-                index = 0;
-                bestMoves[index] = i;
-            } else if (possibleScore === bestPossibleScore) {
-                index++;
-                bestMoves[index] = i;
+        var bestPossibleScore = -Infinity;
+        for(var i = 0; i < chessBoard.length; i++) {
+            if(chessBoard[i] === 'empty') {
+                chessBoard[i] = 'x';
+                //the more 'o' can get, the happier 'x' becomes
+                bestPossibleScore = Math.max(bestPossibleScore, minimizeScore(chessBoard, depth - 1, Math.max(bestPossibleScore, alpha), beta));
+                chessBoard[i] = 'empty';
             }
-            chessBoard[i] = 'empty';
         }
-    }
-    if(index > 0) {
-        index = Math.floor(Math.random() * (index + 1));
-    }
-    return bestMoves[index];
-};
+        return bestPossibleScore;
+    };
 
-// when computer is chosen to be 'o', how should it make its best next move
-var oMiniMax = function(chessBoard, depth) {
-    var bestMoves = new Array(chessBoard.length);
-    var index = 0;
-    var bestPossibleScore = Infinity;
-    for(var i = 0; i < chessBoard.length; i++) {
-        if(chessBoard[i] === 'empty') {
-            chessBoard[i] = 'o';
-            var possibleScore = maximizeScore(chessBoard, depth, -Infinity, Infinity);
-            if(possibleScore < bestPossibleScore) {
-                bestPossibleScore = possibleScore;
-                index = 0;
-                bestMoves[index] = i;
-            } else if (possibleScore === bestPossibleScore) {
-                index++;
-                bestMoves[index] = i;
+    //when the computer is 'o', decide what best possible result it can get in the current situation
+    var minimizeScore = function(chessBoard, depth, alpha, beta) {
+        var gameResult = gameState.getState(chessBoard);
+        var isGameOver = (gameResult === gameState.resultMap.win) || (gameResult === gameState.resultMap.lose) || (gameResult === gameState.resultMap.draw);
+        //alpha cut off
+        if(beta <= alpha) {
+            return gameResult;
+        }
+        if(depth === 0 || isGameOver) {
+            return gameResult;
+        }
+        var bestPossibleScore = Infinity;
+        for(var i = 0; i < chessBoard.length; i++) {
+            if(chessBoard[i] === 'empty') {
+                chessBoard[i] = 'o';
+                //the less 'x' can get, the happier 'o' becomes
+                bestPossibleScore = Math.min(bestPossibleScore, maximizeScore(chessBoard, depth - 1, alpha, Math.min(bestPossibleScore, beta)));
+                chessBoard[i] = 'empty';
             }
-            chessBoard[i] = 'empty';
         }
-    }
-    if(index > 0) {
-        index = Math.floor(Math.random() * (index + 1));
-    }
-    return bestMoves[index];
-};
+        return bestPossibleScore;
+    };
 
-module.exports = function() {
+    // when computer is chosen to be 'x', how should it make its best next move
+    var xMiniMax = function(chessBoard, depth) {
+        var bestMoves = new Array(chessBoard.length);
+        var index = 0;
+        var bestPossibleScore = -Infinity;
+        for(var i = 0; i < chessBoard.length; i++) {
+            if(chessBoard[i] === 'empty') {
+                chessBoard[i] = 'x';
+                var possibleScore = minimizeScore(chessBoard, depth, -Infinity, Infinity);
+                if(possibleScore > bestPossibleScore) {
+                    bestPossibleScore = possibleScore;
+                    index = 0;
+                    bestMoves[index] = i;
+                } else if (possibleScore === bestPossibleScore) {
+                    index++;
+                    bestMoves[index] = i;
+                }
+                chessBoard[i] = 'empty';
+            }
+        }
+        if(index > 1) {
+            index = Math.floor(Math.random() * index) + 1;
+        }
+        return bestMoves[index];
+    };
+
+    // when computer is chosen to be 'o', how should it make its best next move
+    var oMiniMax = function(chessBoard, depth) {
+        var bestMoves = new Array(chessBoard.length);
+        var index = 0;
+        var bestPossibleScore = Infinity;
+        for(var i = 0; i < chessBoard.length; i++) {
+            if(chessBoard[i] === 'empty') {
+                chessBoard[i] = 'o';
+                var possibleScore = maximizeScore(chessBoard, depth, -Infinity, Infinity);
+                if(possibleScore < bestPossibleScore) {
+                    bestPossibleScore = possibleScore;
+                    index = 0;
+                    bestMoves[index] = i;
+                } else if (possibleScore === bestPossibleScore) {
+                    index++;
+                    bestMoves[index] = i;
+                }
+                chessBoard[i] = 'empty';
+            }
+        }
+        if(index > 1) {
+            index = Math.floor(Math.random() * index) + 1;
+        }
+        return bestMoves[index];
+    };
+
     return {
         firstHandMove: xMiniMax;
         secondHandMove: oMiniMax;
     }
-};
+}]);
